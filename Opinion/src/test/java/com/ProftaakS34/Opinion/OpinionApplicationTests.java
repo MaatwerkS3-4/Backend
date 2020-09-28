6d0dbff4dc3078c7732afb0b19d539b683ccae2a
Wesley <wesley.verhagen@student.fontys.nl>
2020-09-28 12:02:01 +0200

    test: check for CI functionality

--- Opinion/src/test/java/com/ProftaakS34/Opinion/OpinionApplicationTests.java
+++ Opinion/src/test/java/com/ProftaakS34/Opinion/OpinionApplicationTests.java
@@ -2,12 +2,14 @@ package com.ProftaakS34.Opinion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class OpinionApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(false);
 	}
 
 }
