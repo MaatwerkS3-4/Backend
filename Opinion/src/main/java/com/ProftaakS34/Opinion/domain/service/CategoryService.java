d1556970fe665e676a3bcc4e768490d22325edc0
Ruben <24549041+RDieleman@users.noreply.github.com>
2021-01-04 13:23:56 +0100

    changed category endpoint to include reverse recommendation

--- Opinion/src/main/java/com/ProftaakS34/Opinion/domain/service/CategoryService.java
+++ Opinion/src/main/java/com/ProftaakS34/Opinion/domain/service/CategoryService.java
@@ -3,7 +3,9 @@ package com.ProftaakS34.Opinion.domain.service;
 import com.ProftaakS34.Opinion.domain.model.Category;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,5 +15,25 @@ public class CategoryService {
         return Stream.of(Category.values())
                 .map(Category::name)
                 .collect(Collectors.toList());
+    }
+
+    public Map<Category, Category> getReverseRecommendations(){
+        Map<Category, Category> reverse = new HashMap<>();
+
+        reverse.put(Category.Business, Category.Sport);
+        reverse.put(Category.Education, Category.Politics);
+        reverse.put(Category.Entertainment, Category.Business);
+        reverse.put(Category.Food, Category.Literature);
+        reverse.put(Category.Health, Category.History);
+        reverse.put(Category.History, Category.Entertainment);
+        reverse.put(Category.Literature, Category.Health);
+        reverse.put(Category.Philosophy, Category.Technology);
+        reverse.put(Category.Psychology, Category.Science);
+        reverse.put(Category.Science, Category.Psychology);
+        reverse.put(Category.Technology, Category.Philosophy);
+        reverse.put(Category.Sport, Category.Education);
+        reverse.put(Category.Politics, Category.Philosophy);
+
+        return reverse;
     }
 }
--- Opinion/src/main/java/com/ProftaakS34/Opinion/web/api/controller/CategoryController.java
+++ Opinion/src/main/java/com/ProftaakS34/Opinion/web/api/controller/CategoryController.java
@@ -1,17 +1,20 @@
 package com.ProftaakS34.Opinion.web.api.controller;
 
+import com.ProftaakS34.Opinion.domain.model.Category;
 import com.ProftaakS34.Opinion.domain.service.CategoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.security.KeyPair;
+import java.util.*;
 
 @RestController
 @RequestMapping("/category")
@@ -39,6 +42,28 @@ public class CategoryController {
     @GetMapping
     public ResponseEntity<List<String>> getCategories(){
         List<String> resource = categoryService.getAvailableCategories();
+        return ResponseEntity.ok(resource);
+    }
+
+    /**
+     * Get reverse recommendations for every category
+     * @return A list of sets with a category and a recommended reverse category
+     */
+    @ApiOperation(
+            value = "Get a reverse recommendation for every category"
+    )
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "Reverse recommendations have been returned")
+    })
+    @GetMapping(path = "/reverse")
+    public ResponseEntity<List<Pair<String, String>>> getReverseRecommendations(){
+        Map<Category, Category> reverse = categoryService.getReverseRecommendations();
+        List<Pair<String, String>> resource = new ArrayList<>();
+
+        for(Map.Entry<Category, Category> entry : reverse.entrySet()){
+            resource.add(Pair.of(entry.getKey().name(), entry.getValue().name()));
+        }
+
         return ResponseEntity.ok(resource);
     }
 }
