81e9c264bf971c81922fd15f22adbaa95d470099
Dongen,Cyrion C.M.N. van <cyrion.vandongen@student.fontys.nl>
2021-01-15 20:11:36 +0100

    Add tests CategoryService

---
+++ Opinion/src/test/java/com/ProftaakS34/Opinion/CategoryServiceTests.java
@@ -0,0 +1,35 @@
+package com.ProftaakS34.Opinion;
+
+import com.ProftaakS34.Opinion.domain.model.Category;
+import com.ProftaakS34.Opinion.domain.service.CategoryService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class CategoryServiceTests {
+    private CategoryService categoryService;
+
+    private CategoryServiceTests() {
+        categoryService = new CategoryService();
+    }
+
+    @Test
+    void GetAvailableTest() {
+        List<String> pulledCats = categoryService.getAvailableCategories();
+
+        Assertions.assertEquals(13, pulledCats.size());
+        Assertions.assertEquals("Technology", pulledCats.get(0));
+        Assertions.assertEquals("Food", pulledCats.get(6));
+    }
+
+    @Test
+    void GetReversesTest() {
+        Map<Category, Category> pulledReverses = categoryService.getReverseRecommendations();
+
+        Assertions.assertEquals(13, pulledReverses.size());
+        Assertions.assertEquals(Category.Sport, pulledReverses.get(Category.Business));
+        Assertions.assertNotEquals(Category.Sport, pulledReverses.get(Category.Philosophy));
+    }
+}
